bd4c21df46aedd3eb013a575323d2f817756d168
ryan <ryan1210.chiu@gmail.com>
2026-08-18 18:59:22 +0800

    Remove bin directory from git tracking and add to .gitignore

--- desktopPet/obj/Debug/net8.0-windows/desktopPet.AssemblyInfo.cs
+++ desktopPet/obj/Debug/net8.0-windows/desktopPet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desktopPet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beae6db1b5b18c81854ba01c7ac62c0979f4e6a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ba6dc5351ecdfea8489ffd1b3390e724608a6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("desktopPet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desktopPet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
